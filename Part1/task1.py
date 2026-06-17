1d272dad66aab55a8b9ca70ce19c4642b0340221
Kasim Hussain <s225165@dtu.dk>
2026-06-17 10:49:14 +0200

    task1

--- Part1/task1.py
+++ Part1/task1.py
@@ -1,3 +1,5 @@
+import math
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -11,3 +13,53 @@ P = [
     [0,      0,     0,      0.991, 0.009],
     [0,      0,     0,      0,     1]
 ]
+
+def simulate_event(P, initial_state):
+    states = [initial_state]
+    months = 0
+    while states[-1] != 4:
+        months += 1
+        current_state = states[-1]
+        next_state = np.random.choice(len(P), p=P[current_state])
+        states.append(next_state)
+    return states, months
+
+def simulate_patients(P, num_patients, initial_state):
+    all_states = []
+    all_months = []
+    for _ in range(num_patients):
+        states, months = simulate_event(P, initial_state)
+        all_states.append(states)
+        all_months.append(months)
+    return all_states, all_months
+
+def task1():
+    initial_state = 1
+    death_months = simulate_patients(P, 1000, initial_state)[1]
+    death_years = np.round(np.array(death_months) / 12, 1)
+    print(np.max(death_months))
+    print(np.min(death_months))
+    print(np.mean(death_months))
+
+    print(np.max(death_years))
+    print(np.min(death_years))
+    print(np.mean(death_years))
+
+    plt.hist(death_years, bins=range(0, int(max(death_years) + 1), 1), edgecolor='black')
+    plt.title('Distribution of Years Until Death')
+    plt.xlabel('Years Until Death')
+    plt.ylabel('Number of Patients')
+    plt.grid(axis='y', alpha=0.75)
+    plt.show()
+
+    plt.hist(death_months, bins=20, edgecolor='black')
+    plt.title('Distribution of Months Until Death')
+    plt.xlabel('Months Until Death')
+    plt.ylabel('Number of Patients')
+    plt.grid(axis='y', alpha=0.75)
+    plt.show()
+
+
+if __name__ == "__main__":
+    task1() #TODO find proportion of state 1 to state 2 
+
